168f06e20bcb69534d77e378a200e3ecb2a629e4
hammad1 <hammad@etu.esisar.grenoble-inp.fr>
2025-08-07 23:20:31 +0200

    Update LED blink example

--- Example/source/blink_led.c
+++ Example/source/blink_led.c
@@ -9,3 +9,18 @@
 /*=============================================================================
 **                                 Includes
 =============================================================================*/
+#include "blink_led.h"
+#include "gpio.h"
+
+
+void Blink_Led_Button_Press()
+{
+    	if (Gpio_Pin_Read(PORT_C, PIN_13) == 0)  // Button is pressed (active low)
+        {
+            Pin_High(PORT_A, PIN_5);  // Turn on LED
+        }
+        else
+        {
+            Pin_Low(PORT_A, PIN_5);   // Turn off LED
+        }
+}
