2856d0e8b84f58494a25ed040f6e54756e9c11a5
hammad1 <hammad@etu.esisar.grenoble-inp.fr>
2025-08-07 23:18:15 +0200

    Add GPIO driver implementation

--- Drivers/include/gpio.h
+++ Drivers/include/gpio.h
@@ -1,16 +1,140 @@
-/*============================== (c) 2025 ===================================
-** File Name   :  main.h                                                **
-** Author      :  Hammad.                                                  **
-** Created on  :  Aug 02, 2025                                               **
-**---------------------------------------------------------------------------**
-** Description : Usefull functions and variables...                          **
-=============================================================================*/
-#ifndef _UTILITIES_H_
-#define _UTILITIES_H_
+/******************************************************************************
+* FILENAME :gpio.h
+* DESCRIPTION : Header file of gpio.c conatains gpio function prototypes, macros
+                structures, enums
+* AUTHOR : Hammad
+******************************************************************************/
 
-/*=============================================================================
-**                                 Includes                                  **
-=============================================================================*/
+#ifndef _GPIO_H_
+#define _GPIO_H_
+
+/********** Includes *********************************************************/
+
+#include "clock.h"
+#include "std_types.h"
+#include "nvic.h"
+
+/********** Macros ***********************************************************/
+
+#define PIN_SEVEN   (0x7U)
+#define PIN_TOGGLE  (0x20U)
+#define EXTI_BASEADDR							(APB2PERIPH_BASEADDR + 0x0400) // EXTI peripheral base address on APB2 bus
+#define APB2PERIPH_BASEADDR    					0x40010000U			// APB2 bus starting address
+#define EXTI ((EXTI_RegDef_t*) EXTI_BASEADDR)  						// EXTI register structure pointer
+#define AFIO_BASEADDR							(APB2PERIPH_BASEADDR)	
+#define AFIO ((AFIO_RegDef_t*) AFIO_BASEADDR) 
+#define AFIO_EN   0
+/********** STRUCTURES *******************************************************/
+
+typedef struct 
+{
+    volatile uint32 CRL;
+    volatile uint32 CRH;
+    volatile uint32 IDR;
+    volatile uint32 ODR;
+    volatile uint32 BSRR;
+    volatile uint32 BRR;
+    volatile uint32 LOCKR;
+} gpio_type;
+
+/********** ENUMS *******************************************************/
+
+typedef enum
+{
+    PORT_A = 0,
+	PORT_B,
+	PORT_C,
+	PORT_D,
+	PORT_E
+} port_type;
+
+typedef enum
+{
+    PIN_0 = 0,
+    PIN_1,
+    PIN_2,
+    PIN_3,
+    PIN_4,
+    PIN_5,
+    PIN_6,
+    PIN_7,
+    PIN_8,
+    PIN_9,
+    PIN_10,
+    PIN_11,
+    PIN_12,
+    PIN_13,
+    PIN_14,
+    PIN_15
+} gpio_pin_num;
+
+typedef enum
+{
+    Input_Analog_Mode = 0x0,
+    SPEED_10_GENERAL_PURPOSE_OUTPUT_PUSHPULL, 
+    SPEED_02_GENERAL_PURPOSE_OUTPUT_PUSHPULL,
+    SPEED_50_GENERAL_PURPOSE_OUTPUT_PUSHPULL, 
+    INPUT_FLOATING,
+    SPEED_10_GENERAL_PURPOSE_OUTPUT_OPENDRAIN,
+    SPEED_02_GENERAL_PURPOSE_OUTPUT_OPENDRAIN,
+    SPEED_50_GENERAL_PURPOSE_OUTPUT_OPENDRAIN,
+    INPUT_PULLUP_PULLDOWN,
+    SPEED_10_ALTERNATE_FUNCTION_OUTPUT_PUSHPULL,
+    SPEED_02_ALTERNATE_FUNCTION_OUTPUT_PUSHPULL,
+    SPEED_50_ALTERNATE_FUNCTION_OUTPUT_PUSHPULL,
+    INPUT_RESERVED,
+    SPEED_10_ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN,
+    SPEED_02_ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN,
+    SPEED_50_ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN,
+} mode_type;
+
+/**
+* @brief  Interrupt Edge selection enum 
+*/
+typedef enum
+{
+	INT_RISING_EDGE,
+	INT_FALLING_EDGE,
+	INT_RISING_FALLING_EDGE
+}int_edge_sel_t;
+
+typedef struct
+{
+	volatile uint32 IMR; 				// Interrupt mask register							Address offset:0x00
+	volatile uint32 EMR;				// Event mask register								Address offset:0x04
+	volatile uint32 RTSR;				// Rising trigger selection register				Address offset:0x08
+	volatile uint32 FTSR;				// Falling trigger selection register				Address offset:0x0c
+	volatile uint32 SWIER;			// Software interrupt event register				Address offset:0x10
+	volatile uint32 PR;				// Pending register									Address offset:0x14
+}EXTI_RegDef_t;
+
+/*
+ * Peripheral register definition structure for AFIO
+ */
+typedef struct
+{
+	volatile uint32 EVCR;				// Event control register 							Address offset:0x00
+	volatile uint32 MAPR;				// AF remap and debug I/O configuration register	Address offset:0x04
+	volatile uint32 EXTICR[4];		// External interrupt configuration registers 		Address offset:0x08-0x14
+	volatile uint32 MAPR2;			// AF remap and debug I/O configuration register2	Address offset:0x18
+}AFIO_RegDef_t;
 
 
-#endif /* _UTILITIES_H_ */
+/********** External APIs ****************************************************/
+
+extern void Gpio_Init (
+                        port_type selected_port,
+                        gpio_pin_num pin_num,
+                        peripheral_clock_type peripherals,
+                        mode_type mode
+);
+extern void Pin_High (port_type selected_port, gpio_pin_num pin_num);
+extern void Pin_Low (port_type selected_port, gpio_pin_num pin_num);
+extern void Pin_Toggle (port_type selected_port, gpio_pin_num pin_num);
+void Gpio_Configure_Interrupt(uint16 pin_no, int_edge_sel_t edge_sel, port_type port);
+void gpio_enable_interrupt(uint16 pin_no, IRQn_Type irq_no);
+void gpio_clear_interrupt(uint16 pin);
+
+uint8 Gpio_Pin_Read(port_type port, gpio_pin_num pin);
+
+#endif /* End of _GPIO_H_ */
--- Drivers/source/gpio.c
+++ Drivers/source/gpio.c
@@ -1,11 +1,138 @@
-/*============================== (c) 2025 ===================================
-** File Name   :  main.c
-** Author      :  Hammad.
-** Created on  :  Aug 02, 2025
-*---------------------------------------------------------------------------*
-** Description : 
-=============================================================================*/
+/******************************************************************************
+* FILENAME :gpio.c
+* DESCRIPTION : Gpio driver functions are being implemented in this file.
+* AUTHOR : Hammad
+******************************************************************************/
 
-/*=============================================================================
-**                                 Includes
-=============================================================================*/
+/********** includes *********************************************************/
+
+#include "gpio.h"
+#include "clock.h"
+
+/********** Array of gpio adresses ******************************************/
+
+static gpio_type * port_address [5U] =
+{
+   ((gpio_type*) (0x40010800U)),
+   ((gpio_type*) (0x40010C00U)),
+   ((gpio_type*) (0x40011000U)),
+   ((gpio_type*) (0x40011400U)),
+   ((gpio_type*) (0x40011800U))
+};
+
+/**
+ * @brief Gpio_Init : configuration of gpio in all the modes
+ * @param selected_port, pin_num, port, mode
+ * @return void
+ */
+void Gpio_Init (port_type selected_port, gpio_pin_num pin_num, peripheral_clock_type peripherals, mode_type mode)
+{
+   Rcc_Enabler(peripherals);
+
+   if (pin_num > PIN_SEVEN)
+   {
+      /*configuraton of pins above pin 7*/
+	   port_address[selected_port]->CRH  &= ~(0xfU << (4 *(pin_num-8)));
+	   port_address[selected_port]->CRH  |=  (mode << (4 *(pin_num-8)));
+   }
+   else
+   {
+    	port_address[selected_port]->CRL  &= ~(0xfU << (4 * pin_num));
+    	port_address[selected_port]->CRL  |=  (mode << (4 * pin_num));
+   }
+}
+  
+void Pin_High (port_type selected_port, gpio_pin_num pin_num)
+{
+	port_address[selected_port]->ODR |= (HIGH << pin_num);
+}
+
+void Pin_Low (port_type selected_port, gpio_pin_num pin_num)
+{
+	port_address[selected_port]->ODR &= ~(HIGH << pin_num);
+}
+
+
+/**
+ * @brief Pin_Toggle : Making pin High/Low 
+ * @param selected_port, pin_num
+ * @param pin_num
+ * @return void
+ */
+void Pin_Toggle (port_type selected_port, gpio_pin_num pin_num)
+{
+	port_address[selected_port]->ODR ^= PIN_TOGGLE;
+}
+
+/**
+ * @brief Gpio_Pin_Read:Input from user to make pin high low
+ * @param selected_port, pin_num
+ * @param pin_num 
+ * @return void
+ */
+
+uint8 Gpio_Pin_Read(port_type port, gpio_pin_num pin)
+{
+    return (port_address[port]->IDR & (1U << pin)) ? 1 : 0;
+}
+
+void Gpio_Configure_Interrupt(uint16 pin_no, int_edge_sel_t edge_sel, port_type port)
+{
+    // Configure edge trigger
+    if (edge_sel == INT_RISING_EDGE)
+        EXTI->RTSR |= (1U << pin_no);
+    else if (edge_sel == INT_FALLING_EDGE)
+        EXTI->FTSR |= (1U << pin_no);
+    else if (edge_sel == INT_RISING_FALLING_EDGE)
+    {
+        EXTI->RTSR |= (1U << pin_no);
+        EXTI->FTSR |= (1U << pin_no);
+    }
+
+    // Enable AFIO clock
+    Rcc_Enabler(AFIO_EN);
+
+    // Configure EXTIx to map to correct port in EXTICR
+    uint8 exti_index = pin_no / 4;
+    uint8 exti_pos = (pin_no % 4) * 4;
+
+    AFIO->EXTICR[exti_index] &= ~(0xF << exti_pos);  // Clear
+    AFIO->EXTICR[exti_index] |= (port << exti_pos);  // Set port
+
+    // Unmask interrupt
+    EXTI->IMR |= (1U << pin_no);
+}
+// void Gpio_Configure_Interrupt(uint16 pin_no, int_edge_sel_t edge_sel)
+// {
+//    if (edge_sel == INT_RISING_EDGE )
+// 	{
+// 		EXTI->RTSR |= 1 << pin_no;
+// 	}	
+// 	else if (edge_sel == INT_FALLING_EDGE)
+// 	{
+// 		EXTI->FTSR |= 1 << pin_no;
+// 	}else if (edge_sel == INT_RISING_FALLING_EDGE )
+// 	{
+// 		EXTI->FTSR |= 1 << pin_no;
+// 		EXTI->RTSR |= 1 << pin_no;
+// 	}
+// 	else
+// 	{
+// 		 ;//TODO
+// 	}
+// }
+
+
+void gpio_enable_interrupt(uint16 pin_no, IRQn_Type irq_no)
+{
+   EXTI->IMR |= 1 << pin_no;
+	NVIC_EnableIRQ(irq_no);
+}
+
+void gpio_clear_interrupt(uint16 pin)
+{
+   if(EXTI->PR & (1 << pin ))
+	{
+		EXTI->PR |= 1 << pin;
+	}
+}
