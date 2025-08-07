63ccc73509ec0555b2184549628a14b33b0be414
hammad1 <hammad@etu.esisar.grenoble-inp.fr>
2025-08-07 23:21:33 +0200

    Add standard types definition

---
+++ Drivers/include/std_types.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+* FILENAME : std_types.h
+* DESCRIPTION : This header file contain all the data types
+* AUTHOR : Hammad
+******************************************************************************/
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+/********** Data Types ********************************************************/
+
+typedef unsigned char 			uint8;
+typedef unsigned short 			uint16;
+typedef unsigned long 			uint32;
+
+#endif /* STD_TYPES_H_ */
